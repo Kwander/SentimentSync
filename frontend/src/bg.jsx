5acbf0b7f383cbc6a367c215ad98e0bceac58c2b
voltiyo <azizsafouane167@gmail.com>
2024-10-28 09:26:26 +0100

    bg change

--- frontend/src/bg.jsx
+++ frontend/src/bg.jsx
@@ -2,6 +2,6 @@ import "./bg.css";
 
 export default function Background(){
     return(
-        <video id="vidbg" type="video/mp4" src="https://clientdemand.onrender.com/material/bg.mp4" autoPlay muted loop ></video>
+        <video id="vidbg" type="video/mp4" src="https://clientdemand.onrender.com/material/resultbg.mp4" autoPlay muted loop ></video>
     )
 }
